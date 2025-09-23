7e03f213f21a5a18ad38a7544a228d17c78bbca8
ryekass3 <ryekass3@gmail.com>
2025-09-23 16:08:35 -0400

    update media and some pages
    index beginning slideshow
    style - .container
    story - switch to kiss

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ function showMessage() {
     "I love you ❤️",
     "You make me the happiest person",
     "Can’t wait to see you again 💕",
-    "You’re my forever"
+    "You’re my forever",
+    "I miss you more than you know :(",
+    "I'm counting down till summer",
+    "I can't wait to give you a hug",
+    "You're worth losing any amount of sleep over",
+    "I would give you anything just to see you smile",
+    "I'll always be here for you"
   ];
   const random = messages[Math.floor(Math.random() * messages.length)];
   document.getElementById("surprise").innerText = random;
